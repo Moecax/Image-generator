01b8f8278614c1f9870dbe78f04fd530966aed1e
Moecax <moseskwereban@gmail.com>
2022-12-22 11:01:52 +0300

    front end complete

--- routes/openaiRoutes.js
+++ routes/openaiRoutes.js
@@ -1,6 +1,7 @@
-const express = require('express');
-const {generateImage} = require('../controllers/openaiController');
+const express = require("express");
+const { generateImage } = require("../controllers/openaiController");
 const router = express.Router();
 
-router.post('/genrerateimage', generateImage);
+router.post("/generateimage", generateImage);
+
 module.exports = router;
